01d74e278d3c219f14debf530841e10ecf37f6c6
Cedrenou <ced2411@gmail.com>
2025-02-07 17:42:39 +0100

    commit for push

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,6 +5,7 @@ import AddGameForm from '../components/AddGameForm';
 const AdminPage: React.FC = () => {
     return (
         <div className="container mx-auto p-4">
+
             <h1 className="text-3xl font-bold mb-6">Administration des jeux</h1>
             <AddGameForm />
         </div>
